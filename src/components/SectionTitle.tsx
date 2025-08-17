586502b9206c96d4e83b0f95c6ab26da82c49026
rafnas <rafnaspp4@gmail.com>
2025-08-17 23:47:02 +0530

    fixes

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -6,17 +6,9 @@ import { motion } from "framer-motion";
 
 interface SectionHeaderProps {
   children: React.ReactNode;
-  color?: string;
-  size?: string;
-  className?: string;
 }
 
-export function SectionHeader({
-  children,
-  color = "text-[#00989D]",
-  size = "text-3xl md:text-4xl",
-  className = "",
-}: SectionHeaderProps) {
+export function SectionHeader({ children }: SectionHeaderProps) {
   return (
     <>
       <motion.h2 className="text-2xl sm:text-2xl font-bold text-teal-800 text-center leading-snug">
