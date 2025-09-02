528588da2d360a02474154773ae72a6b4ef88f35
rafnas <rafnaspp4@gmail.com>
2025-09-02 13:29:17 +0530

    fixed

--- src/components/feedback/feedback-form.tsx
+++ src/components/feedback/feedback-form.tsx
@@ -126,8 +126,7 @@ const FeedbackForm = ({ encodedData }: FeedbackFormProps) => {
             Share Your Experience
           </h1>
           <p className="text-teal-100 text-sm">
-            {userData.name ? `Hi ${userData.name}!` : "Hi there!"} We had love to
-            hear about your session
+            We had love to hear about your session
           </p>
         </div>
 
