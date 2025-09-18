8a595401580a518151d34bf047c34a0dd90b040c
rafnas <rafnaspp4@gmail.com>
2025-09-19 04:06:49 +0530

    fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 // import HeroMob from "./HeroMob";
 // import HeroDesktop from "./HeroDesktop";
 
 const Hero = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
 
   // Detect screen size on mount & window resize
-  useEffect(() => {
-    const checkScreenSize = () => {
-      setIsMobile(window.innerWidth < 768); // Adjust breakpoint as needed
-    };
+  // useEffect(() => {
+  //   const checkScreenSize = () => {
+  //     setIsMobile(window.innerWidth < 768); // Adjust breakpoint as needed
+  //   };
 
-    checkScreenSize(); // Initial check
-    window.addEventListener("resize", checkScreenSize);
+  //   checkScreenSize(); // Initial check
+  //   window.addEventListener("resize", checkScreenSize);
 
-    return () => window.removeEventListener("resize", checkScreenSize);
-  }, []);
+  //   return () => window.removeEventListener("resize", checkScreenSize);
+  // }, []);
 
   return (
     <div className="bg-[#F7F8F2]">
