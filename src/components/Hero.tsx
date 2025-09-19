0bc8eb21a5794e335e4ce86d9048a6df1e59472c
rafnas <rafnaspp4@gmail.com>
2025-09-19 16:19:13 +0530

    normal site

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,23 @@
-import React from "react";
-// import HeroMob from "./HeroMob";
-// import HeroDesktop from "./HeroDesktop";
+import React, { useEffect, useState } from "react";
+import HeroMob from "./HeroMob";
+import HeroDesktop from "./HeroDesktop";
 
 const Hero = () => {
-  // const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   // Detect screen size on mount & window resize
-  // useEffect(() => {
-  //   const checkScreenSize = () => {
-  //     setIsMobile(window.innerWidth < 768); // Adjust breakpoint as needed
-  //   };
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust breakpoint as needed
+    };
 
-  //   checkScreenSize(); // Initial check
-  //   window.addEventListener("resize", checkScreenSize);
+    checkScreenSize(); // Initial check
+    window.addEventListener("resize", checkScreenSize);
 
-  //   return () => window.removeEventListener("resize", checkScreenSize);
-  // }, []);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
 
-  return (
-    <div className="bg-[#F7F8F2]">
-      <h1>🚧 Site Under Maintenance 🚧</h1>
-    </div>
-  );
+  return <div className="bg-[#F7F8F2]">{isMobile ? <HeroMob /> : <HeroDesktop />}</div>;
 };
 
 export default Hero;
