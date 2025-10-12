49e1fe4f2b210ee651c9c429eedcd718ac3d7a74
rafnas <rafnaspp4@gmail.com>
2025-10-12 14:55:23 +0530

    fixed

--- src/components/WhatsappChat/whatsapp-chat.tsx
+++ src/components/WhatsappChat/whatsapp-chat.tsx
@@ -15,7 +15,7 @@ export function WhatsAppChat() {
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Chat on WhatsApp"
-      className="fixed z-50 bottom-4 right-4 md:bottom-6 md:right-6 h-24 w-24 rounded-full bg-green text-primary-foreground shadow-lg hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring flex items-center justify-center"
+      className="fixed z-50 bottom-4 right-4 md:bottom-6 md:right-6 h-24 w-24 bg-green text-primary-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring flex items-center justify-center"
     >
    <img src="/green-whatsapp-icon-with-phone-it.png" />
       <span className="sr-only">Chat on WhatsApp</span>
