0e8cc863e856ee0466f376f60cf009b3fbb9c975
rafnas <rafnaspp4@gmail.com>
2025-11-20 23:33:32 +0530

    fixed

--- src/components/Swiper/index.tsx
+++ src/components/Swiper/index.tsx
@@ -164,7 +164,7 @@ export default function Carousel3DFixedTiming({
                         {psychologist.name || "Unknown Psychologist"}
                       </h2>
                       <p className="text-teal/90 text-sm mb-2">
-                        {psychologist.specialization || "General Psychology"}
+                        {psychologist.designation || "General Psychology"}
                       </p>
                     </>
                     <motion.button
