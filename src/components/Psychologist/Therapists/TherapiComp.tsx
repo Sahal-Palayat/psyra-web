036c1b2e2e4a9845358947e50a9e4da29f1bc8c0
rafnas <rafnaspp4@gmail.com>
2025-11-20 22:54:55 +0530

    fixed

--- src/components/Psychologist/Therapists/TherapiComp.tsx
+++ src/components/Psychologist/Therapists/TherapiComp.tsx
@@ -129,7 +129,7 @@ export default function TherapistsCard() {
       );
 
       const list = Array.isArray(response?.data?.psychologists)
-        ? response.data?.psychologists
+        ? response?.data?.psychologists
         : [];
       setAllData(list);
       setData(
