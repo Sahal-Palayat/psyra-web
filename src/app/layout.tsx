3c0089eeb5c313fe45b9a328798f6f81c5992a7e
AnjaliAS221 <anjalias342@gmail.com>
2026-01-14 18:43:01 +0530

    added gtm code and commented the ga script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Onest, Geist_Mono } from "next/font/google"
+import "./globals.css";
+import type { Metadata } from "next";
+import { Onest, Geist_Mono } from "next/font/google";
 
 const onest = Onest({
   variable: "--font-onest",
@@ -12,8 +12,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-
 
 export const metadata: Metadata = {
   title: "Online Counseling Psychology",
@@ -100,8 +98,6 @@ export const metadata: Metadata = {
   },
 };
 
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -139,24 +135,45 @@ export default function RootLayout({
             }),
           }}
         />
-        <script
+        {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-53R46XKX4Y"
-        />
-        <script id="google-analytics">
+        /> */}
+        {/* <script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-53R46XKX4Y');
           `}
-        </script>
+        </script> */}
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5QRB9K55');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${onest.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QRB9K55"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <div id="modal-root" />
       </body>
     </html>
   );
 }
-
